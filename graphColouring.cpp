91187f54fc71582c807730bf23427465024e3989
KSIX2 <welovegames@gmail.com>
2021-11-17 17:46:56 +0400

    user inp

--- graphColouring.cpp
+++ graphColouring.cpp
@@ -19,8 +19,8 @@ graph::graph(int num) {
 }
 
 void graph::addEdge(int v1, int v2) {
-    adjVertices[v1].push_back(v2);   
-    adjVertices[v2].push_back(v1); 
+    adjVertices[v1].push_back(v2);
+    adjVertices[v2].push_back(v1);
 }
 
 void graph::greedyColouring() {
@@ -28,57 +28,43 @@ void graph::greedyColouring() {
 
     for(int vertex = 1; vertex < numberOfVertices; ++vertex) { //iterate through the remaining vertices in graph
         std::vector<bool> usedColours(numberOfVertices, false);
-    
+
         std::vector<int>::iterator adjVertex;
         for(adjVertex = adjVertices[vertex].begin(); adjVertex != adjVertices[vertex].end(); ++adjVertex) //iterate through all adjacent vertices of a vertex
             if(colours[*adjVertex] != -1)
                 usedColours[colours[*adjVertex]] = true;
-    
+
         int minColour;
-        for(minColour = 0; usedColours[minColour] != false; ++minColour); 
-    
+        for(minColour = 0; usedColours[minColour] != false; ++minColour);
         colours[vertex] = minColour;
     }
 }
 
 void graph::displayColours() {
-    for(int vertex = 0; vertex < numberOfVertices; ++vertex)
-        std::cout << "Vertex " << vertex << " : " << "colour " << colours[vertex] << std::endl; 
+    for(int vertex = 1; vertex <= numberOfVertices; ++vertex)
+        std::cout << "Vertex " << vertex << " : " << "colour " << colours[vertex-1] << std::endl;
 }
 
 int main()
 {
-    graph t(13), g(8);
-    t.addEdge(0, 1);
-    t.addEdge(1, 2);
-    t.addEdge(1, 7);
-    t.addEdge(1, 8);
-    t.addEdge(2, 3);
-    t.addEdge(2, 6);
-    t.addEdge(3, 4);
-    t.addEdge(3, 5);
-    t.addEdge(8, 9);
-    t.addEdge(8, 12);
-    t.addEdge(9, 10);
-    t.addEdge(9, 11);
+    int nov, noe;
+    std::cout << "Enter number of vertices (order) of the graph - ";
+    std::cin >> nov;
+    std::cout << "Enter number of edges (size) of the graph - ";
+    std::cin >> noe;
 
-    std::cout << "\nExample Tree graph colouring: ";
+    graph t(nov);
+
+    int u, v, i = 0;
+    while(i<noe){
+        std::cout << "Enter the pair to be linked (separated by space) - ";
+        std::cin >> u;
+        std::cin >> v;
+        t.addEdge(u, v);
+        i++;
+    }
+
     t.greedyColouring();
     t.displayColours();
-    
-    g.addEdge(0,1);
-    g.addEdge(1,2);
-    g.addEdge(1,3);
-    g.addEdge(2,4);
-    g.addEdge(2,5);
-    g.addEdge(3,6);
-    g.addEdge(4,7);
-    g.addEdge(5,7);
-    g.addEdge(6,5);
-    g.addEdge(6,7);
-    
-    std::cout << "\nExample Graph graph colouring: ";
-    g.greedyColouring();
-    g.displayColours();
-    return 0;    
+    return 0;
 }
